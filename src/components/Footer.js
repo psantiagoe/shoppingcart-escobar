55da8bbc1729c749e2f3e38da2eae6426cbe5bad
santiago.escobar <santiago.escobar1995@gmail.com>
2022-02-23 02:10:48 -0300

    Desafio Menu e-commerce

---
+++ src/components/Footer.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
+
+import logoLarge from "../assets/img/logo_large.png";
+
+const Footer = () => {
+	return (
+		<footer>
+			<Container>
+				<Row className="d-flex align-items-center light">
+					<Col md={6}>
+						<img alt="Foodi logo" src={logoLarge} width={180} height={51} />
+						<p>Copyright © 2022 Foodi</p>
+					</Col>
+					<Col md={6}>
+						<Nav className="justify-content-end link-color">
+							<Nav.Link href="https://www.facebook.com/">
+								<FontAwesomeIcon icon={faFacebook} className="icon fb-icon"></FontAwesomeIcon>
+							</Nav.Link>
+							<Nav.Link href="https://www.instagram.com/">
+								<FontAwesomeIcon icon={faInstagram} className="icon ig-icon"></FontAwesomeIcon>
+							</Nav.Link>
+							<Nav.Link href="https://twitter.com/">
+								<FontAwesomeIcon icon={faTwitter} className="icon tw-icon"></FontAwesomeIcon>
+							</Nav.Link>
+						</Nav>
+					</Col>
+				</Row>
+			</Container>
+		</footer>
+	);
+};
+
+export default Footer;
---
+++ src/components/NavBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import logo from "../assets/img/foodi-logo.svg";
+
+const NavBar = () => {
+	return (
+		<Navbar fixed="top" bg="light" expand="lg">
+			<Container>
+				<Navbar.Brand href="#home">
+					<img alt="Foodi logo" src={logo} className="d-inline-block align-top" />
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="me-auto">
+						<Nav.Link href="#home">Home</Nav.Link>
+						<Nav.Link href="#link">Nuestros locales</Nav.Link>
+						<NavDropdown title="Dropdown" id="basic-nav-dropdown">
+							<NavDropdown.Item href="#action/3.1">Entradas</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.2">Platos Principales</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.3">Postres</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.4">Bebidas</NavDropdown.Item>
+						</NavDropdown>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+};
+
+export default NavBar;
